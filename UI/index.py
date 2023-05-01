cbdecf05e4b3b2f678239d957ced4a354c4a67f4
bhawanaprasain <bhawana.prs@gmail.com>
2023-05-01 12:46:18 +0545

    Add docker for chatbot

--- UI/index.py
+++ UI/index.py
@@ -2,7 +2,7 @@
 import gradio as gr
 import requests
 
-BOT_URL = "http://localhost:5005/webhooks/rest/webhook"  # Replace with your Rasa server's endpoint
+BOT_URL = "http://rasa:5005/webhooks/rest/webhook"  # Replace with your Rasa server's endpoint
 
 with gr.Blocks(css=".gradio-container {background-color: #F0F8FF} .feedback {background-color: 	#00ff00}") as demo:
     chatbot = gr.Chatbot()
@@ -23,5 +23,5 @@ with gr.Blocks(css=".gradio-container {background-color: #F0F8FF} .feedback {bac
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
     clear.click(lambda: None, None, chatbot, queue=False)
 
-demo.launch()
+demo.launch(share=True,debug=True,show_error=True)
 
